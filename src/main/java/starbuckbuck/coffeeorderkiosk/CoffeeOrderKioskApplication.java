babce536d987cec0191c63f11c80c92a2f21b200
jujemu <jujemu30@gmail.com>
2023-07-07 15:21:11 +0900

    Init:

---
+++ src/main/java/starbuckbuck/coffeeorderkiosk/CoffeeOrderKioskApplication.java
@@ -0,0 +1,13 @@
+package starbuckbuck.coffeeorderkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderKioskApplication.class, args);
+    }
+
+}
