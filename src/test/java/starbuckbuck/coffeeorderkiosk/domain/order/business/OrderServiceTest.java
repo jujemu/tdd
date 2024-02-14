2b5512418b957d691ea85ffdb283cdb628363ad5
jujemu <jujemu30@gmail.com>
2024-02-15 06:14:47 +0900

    Feat: 주문에서 재고 관리 로직 추가

--- src/test/java/starbuckbuck/coffeeorderkiosk/domain/order/business/OrderServiceTest.java
+++ src/test/java/starbuckbuck/coffeeorderkiosk/domain/order/business/OrderServiceTest.java
@@ -9,6 +9,8 @@ import starbuckbuck.coffeeorderkiosk.domain.order.presentation.OrderCreateReques
 import starbuckbuck.coffeeorderkiosk.domain.product.Product;
 import starbuckbuck.coffeeorderkiosk.domain.product.ProductType;
 import starbuckbuck.coffeeorderkiosk.domain.product.persistence.ProductRepository;
+import starbuckbuck.coffeeorderkiosk.domain.stock.Stock;
+import starbuckbuck.coffeeorderkiosk.domain.stock.repository.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -17,7 +19,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static starbuckbuck.coffeeorderkiosk.domain.product.ProductType.HANDMADE;
+import static starbuckbuck.coffeeorderkiosk.domain.product.ProductType.*;
 
 @Transactional
 @SpringBootTest
@@ -29,14 +31,21 @@ class OrderServiceTest {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    StockRepository stockRepository;
+
     @DisplayName("상품 목록을 받아 주문을 생성하면 INIT 상태를 가진 주문이 생성된다.")
     @Test
     void createOrder() throws Exception {
         //given
-        Product product1 = createProduct("001", HANDMADE, "아메리카노", 5000);
-        Product product2 = createProduct("002", HANDMADE, "카페라떼", 6000);
+        Product product1 = createProduct("001", BAKERY, "아메리카노", 5000);
+        Product product2 = createProduct("002", BOTTLE, "카페라떼", 6000);
         Product product3 = createProduct("003", HANDMADE, "팥빙수", 8000);
         productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = new Stock("001", 2);
+        Stock stock2 = new Stock("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
 
         Map<String, Integer> productNumberCounter = new HashMap<>();
         productNumberCounter.put("001", 2);
@@ -58,6 +67,14 @@ class OrderServiceTest {
                         tuple("001", "아메리카노"),
                         tuple("001", "아메리카노"),
                         tuple("003", "팥빙수")
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 2)
                 );
     }
 
