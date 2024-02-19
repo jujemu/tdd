ddff5699d738bd837ac16a5d7924f4d23eb6117a
jujemu <jujemu30@gmail.com>
2024-02-19 09:16:33 +0900

    Feat: WebMvcTest 와 EnableJpaAuditing 문제 해결을 위해 구성 분리

--- src/main/java/starbuckbuck/coffeeorderkiosk/CoffeeOrderKioskApplication.java
+++ src/main/java/starbuckbuck/coffeeorderkiosk/CoffeeOrderKioskApplication.java
@@ -2,9 +2,7 @@ package starbuckbuck.coffeeorderkiosk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class CoffeeOrderKioskApplication {
 
